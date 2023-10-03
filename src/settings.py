3d0b41c84e24d150fcb01f28818d2bd26efe47a6
vorozhkog <vorozkhog@icloud.com>
2023-10-03 18:55:41 +0100

    preview image id changed

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/thepbordin/indoor-object-detection/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 6583323
+PREVIEW_IMAGE_ID: int = 6584872
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/indoor-object-detection"
